a1a8e639ee60b15e239dd446a513096c53b9d2fb
Mykyta Opanasiuk <nikitaopanassiuk@outlook.com>
2023-10-07 18:14:23 +0300

    Adding semantic tags and Open Graph Protocol

---
+++ src/Footer.js
@@ -0,0 +1,30 @@
+function Footer() {
+    return (
+        <footer>
+            <img src="/images/Logo.svg" alt="Logo"></img>
+            <p>Doormat Navigation</p>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#">Reservations</a></li>
+                <li><a href="#">Order Online</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+            <p>Contact</p>
+            <ul>
+                <li><a href="#">Address</a></li>
+                <li><a href="#">Phone number</a></li>
+                <li><a href="#">Email</a></li>
+            </ul>
+            <p>Social Media Links</p>
+            <ul>
+                <li><a href="#">Facebook</a></li>
+                <li><a href="#">Instagram</a></li>
+                <li><a href="#">Twitter</a></li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer;
